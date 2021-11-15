b9d227d9f3a54dab8405feb80ff97c5e3b4092b0
Adam Paulukanis <adam.paulukanis@gmail.com>
2021-11-15 11:55:48 +0100

    useEffect

---
+++ Component.js
@@ -0,0 +1,22 @@
+/* jshint strict: global, esversion: 9, browser: true */
+'use strict';
+
+import { useState, useEffect } from './MyReact.js';
+
+export default function Component({ propCount, buttonElem }) {
+  const [count, setCount] = useState(0);
+  const propCountDoubled = 0;
+
+  useEffect(() => {
+    const handler = () => setCount(currentCount => currentCount + 1);
+    buttonElem.addEventListener('click', handler);
+
+    return () => buttonElem.removeEventListener('click', handler);
+  }, [buttonElem]);
+
+  return `
+    State: ${count}
+    Prop: ${propCount}
+    Prop Doubled: ${propCountDoubled}
+  `;
+}
